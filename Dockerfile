e95e940e167b31bf634fc820253714c612f620ee
ElmGone <mrcs.elmgone@mailnull.com>
2016-06-27 11:33:22 +0200

    optional http proxy support ++

--- Dockerfile
+++ Dockerfile
@@ -27,13 +27,16 @@ WORKDIR /go/src
 # add a helper tool written in Go
 ADD gopath gopath
 RUN go install gopath
+RUN GOOS=windows GOARCH=amd64 go install gopath
 
 # add a vendoring tool written in Go
 RUN go get github.com/FiloSottile/gvt
 RUN go install github.com/FiloSottile/gvt
 
 # copy all the helper binaries onto the PATH
-RUN cp /go/bin/* /usr/local/bin
+##RUN cp /go/bin/* /usr/local/bin
+
+##RUN [ -d /local-tools ] && cp /go/bin/gopath /go/bin/windows_amd64/gopath.exe /local-tools
 
 # open port for go doc
 EXPOSE 6060
--- setup.sh
+++ setup.sh
@@ -3,7 +3,38 @@
 # build the docker image
 #
 
-sudo docker build -t elmgone/elmgode .
+if [ "XX$http_proxy" != "XX" ] ; then
+PROXY="--build-arg HTTP_PROXY=$http_proxy \
+	--build-arg http_proxy=$http_proxy \
+	--build-arg HTTPS_PROXY=$http_proxy \
+	--build-arg https_proxy=$http_proxy"
+fi
 
-# sudo docker build -f Dockerfile.gvt -t elmgone/elmgovt .
+sudo docker build -t elmgone/elmgode $PROXY  . || exit 17
 
+
+[ -d elmgode-tools ] || mkdir elmgode-tools
+LT="-v $(pwd)/elmgode-tools:/elmgode-tools"
+
+
+# exit 0
+#	-v "$(pwd)/elmgode-tools:/local-tools" \
+
+
+[ -d elmgode-tools ] || mkdir elmgode-tools
+if sudo docker run \
+	-it --rm \
+	$LT \
+	-e "HOME=/tmp" \
+	-u $(id -u):$(id -g) \
+	elmgone/elmgode \
+	bash -xc "[ -d /elmgode-tools ] && cp /go/bin/gopath /go/bin/windows_amd64/gopath.exe /elmgode-tools" ; then
+
+	echo "SUCCESS!!  please make sure the tools in the folder elmgode-tools are in your PATH"
+	echo
+	cp go.sh elmgode-tools
+	file elmgode-tools/*
+
+else
+	echo "FAILED."
+fi
